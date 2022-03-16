857b77a12e83b4c45fc00bca5593264b55d04390
Chien-Yu-Hsiang <yjian3934@github.com>
2022-03-16 12:15:58 +0100

    My Message

--- choise.py
+++ choise.py
@@ -64,13 +64,13 @@ print(words[secondquestion])
 print("Thirdquestion :")
 print(words[thirdquestion])
 
-#input file
-fin = open("data.txt", "rt")
-#output file to write the result to
-fout = open("index.php", "wt")
-#for each line in the input file
+# input file
+fin = open("data.txt","rt")
+# output file to write the result to
+fout = open("index.php","wt")
+# for each line in the input file
 for line in fin:
     fout.write(line.replace("question1", words[firstquestion]).replace("question2", words[secondquestion]).replace("question3", words[thirdquestion]))
-#close input and output files
+# close input and output files
 fin.close()
 fout.close()
